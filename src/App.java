1dab0540a431c82c32abb720b18d74b1d98d2443
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2022-07-21 01:24:14 -0300

    adicionada a variavel url com o endereço da API public do IMDB

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        // fazer uma conexão HTTP e buscar o TOP 250 filmes
+
+        // extrair só os dados que interessam (titulo, poster, classificação ) <<< isso significa parsear os dados
+
+        // manipular e exibir os dados
+
+        
     }
 }

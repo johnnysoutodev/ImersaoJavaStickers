4bd022e8fe427ee10706396d8620dc6479785b02
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2022-07-21 01:58:07 -0300

    ajustado o nosso connect com o IMDB

--- src/App.java
+++ src/App.java
@@ -1,13 +1,24 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
         // fazer uma conexão HTTP e buscar o TOP 250 filmes
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_cvr5teic";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
 
         // extrair só os dados que interessam (titulo, poster, classificação ) <<< isso significa parsear os dados
 
         // manipular e exibir os dados
 
-        
     }
 }
